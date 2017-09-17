65d896cd2029858d08ded977eefb5b07c86a7b82
Karuhanga Lincoln <karuhanga475@gmail.com>
2017-09-17 06:57:49 +0300

    Initial Commit

---
+++ html/scripts/core.js
@@ -0,0 +1,80 @@
+var TIME_SHORT= 100;
+var TIME_MEDIUM= 200;
+var TIME_LONG= 500;
+
+$(document).ready(function (){
+	onLoad();
+})
+
+function onLoad() {
+	$('#drawer-icon').bind('click', function (){
+		openDrawer();
+	})
+
+	$('#closeDrawer').bind('click', function (){
+		closeDrawer();
+	})
+
+	$('#label-ds').bind('click', function (){
+		toggleMenu($('#ds'));
+	})
+
+	$('#label-search').bind('click', function(){
+		toggleMenu($('#search'));
+	})
+
+	$('#label-sort').bind('click', function(){
+		toggleMenu($('#sort'));
+	})
+
+	$('#fab1').bind('mouseleave', function(){
+		$('#name1').hide(TIME_SHORT);
+	});
+
+	$('#fab1').bind('mouseenter', function(){
+		$('#name1').show(TIME_MEDIUM);
+	});
+
+	$('#fab2').bind('mouseleave', function(){
+		$('#name2').hide(TIME_SHORT);
+	});
+
+	$('#fab2').bind('mouseenter', function(){
+		$('#name2').show(TIME_MEDIUM);
+	});
+
+	$('#fab3').bind('mouseleave', function(){
+		$('#name3').hide(TIME_SHORT);
+	});
+
+	$('#fab3').bind('mouseenter', function(){
+		$('#name3').show(TIME_MEDIUM);
+	});
+}
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openDrawer() {
+    $('#navDrawer').css('width','240px');
+    $('#main').css('margin-left', '240px');
+    $('#content').css('background-color', 'rgba(0,0,0,0.5)');
+    $('#content').bind('click', function (){
+    	closeDrawer();
+    });
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeDrawer() {
+    $('#navDrawer').css('width','0');
+    $('#main').css('margin-left', '0');
+    $('#content').css('background-color', '#f8f8f8');
+    $('#content').unbind('click');
+}
+
+function toggleMenu(menu) {
+	if (menu.css('display')=='none') {
+		menu.show(TIME_MEDIUM);
+	}
+	else{
+		menu.hide(TIME_MEDIUM);
+	}
+}
