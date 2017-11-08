de5bd231dcd1316a0479b5412ac00a4a3df0ffe2
Karuhanga Lincoln <karuhanga475@gmail.com>
2017-11-09 00:13:59 +0300

    Removed Search Icon and Unnecessary Controls

--- Project/app/static/scripts/distinct/sort/quick_sort.js
+++ Project/app/static/scripts/distinct/sort/quick_sort.js
@@ -6,12 +6,10 @@ function showSummary() {
 	$('#container-visualisation-quick_sort').hide();
 	$('#container-data-quick_sort').hide();
 	$('.container-summary-quick_sort').show();
-	hideControls();
 }
 
 function showVisualisation() {
 	$('#container-data-quick_sort').hide();
 	$('.container-summary-quick_sort').hide();
 	$('#container-visualisation-quick_sort').show();
-	showControls();
 }
--- Project/app/static/scripts/distinct/sort/selection_sort.js
+++ Project/app/static/scripts/distinct/sort/selection_sort.js
@@ -6,12 +6,10 @@ function showSummary() {
 	$('#container-visualisation-selection_sort').hide();
 	$('#container-data-selection_sort').hide();
 	$('.container-summary-selection_sort').show();
-	hideControls();
 }
 
 function showVisualisation() {
 	$('#container-data-selection_sort').hide();
 	$('.container-summary-selection_sort').hide();
 	$('#container-visualisation-selection_sort').show();
-	showControls();
 }
